2110effaa4f8525217d10fd5bf979a7b84496b49
jakubmichaluszek <72355148+jakubmichaluszek@users.noreply.github.com>
2023-07-15 17:18:28 +0200

    Add files via upload

---
+++ CSharpLibrary/MedianFilterCSharp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLibrary
+{
+    public class MedianFilterCSharp
+    {
+        //filtrowanie tablicy i zapisanie wyniku w drugiej tablicy
+        public void filtruj(byte[] initialImage, int width, int height, byte[] finalImage)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // Initialize R, G, B to 0
+                    int r = 0, g = 0, b = 0;
+
+                    // Get 3x3 neighborhood around (x, y)
+                    List<int> valuesR = new List<int>();
+                    List<int> valuesG = new List<int>();
+                    List<int> valuesB = new List<int>();
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int xCoord = x + j;
+                            int yCoord = y + i;
+                            if (xCoord >= 0 && xCoord < width && yCoord >= 0 && yCoord < height)
+                            {
+                                valuesR.Add(initialImage[(yCoord * width + xCoord) * 3 + 2]);
+                                valuesG.Add(initialImage[(yCoord * width + xCoord) * 3 + 1]);
+                                valuesB.Add(initialImage[(yCoord * width + xCoord) * 3]);
+                            }
+                        }
+                    }
+
+                    // Get median value of each list and assign to R, G, B
+                    r = getMedian(valuesR);
+                    g = getMedian(valuesG);
+                    b = getMedian(valuesB);
+
+                    // Store R, G, B values in final image
+                    finalImage[(y * width + x) * 3 + 2] = (byte)r;
+                    finalImage[(y * width + x) * 3 + 1] = (byte)g;
+                    finalImage[(y * width + x) * 3] = (byte)b;
+                }
+            }
+        }
+
+        // Returns the median value of a list of integers
+        private int getMedian(List<int> values)
+        {
+            values.Sort();
+            if (values.Count % 2 == 1)
+            {
+                return values[(values.Count - 1) / 2];
+            }
+            else
+            {
+                return (values[values.Count / 2] + values[values.Count / 2 - 1]) / 2;
+            }
+        }
+    }
+}
+
---
+++ DllASM/median.asm
@@ -0,0 +1,39 @@
+﻿; Jakub Michaluszek
+; Grupa 1
+; Filtr medianowy
+; Zwraca medianę w rejestrze RAX
+.data
+
+.code
+; Procedura wylicza medianę wartości z przekazanej tablicy 'values' 
+; Parametry wejsciowe wskaźnik na tablicę values, oraz długość tablicy, przechowywane w rejestrach RCX oraz RDX
+; Parametry wyjsciowe mediana z przekazanej tablicy, przechowywana w rejestrze RAX
+
+; przyjmuje dwa argumenty: wskaźnik na tablicę oraz jej długość
+GetMedian PROC EXPORT
+    ; przekazane argumenty są już dostępne w rejestrach RCX i RDX
+    mov r8, rcx   ; wskaźnik na tablicę jest przekazany w rejestrze RCX
+    mov r9, rdx   ; długość tablicy jest przekazana w rejestrze RDX
+
+    ; oblicz indeks środkowego elementu tablicy
+    mov rax, r9
+    inc rax
+    shr rax, 1
+    mov r10, rax
+
+    ; sprawdź, czy długość tablicy jest parzysta
+    test r9, 1
+    jz parzysta
+
+    ; jeśli długość jest nieparzysta, zwróć wartość środkowego elementu
+    mov rax, [r8 + r10 * 4]
+    ret
+
+parzysta:
+    ; jeśli długość jest parzysta, zwróć średnią wartość dwóch środkowych elementów
+    mov rax, [r8 + r10 * 4]
+    add rax, [r8 + r10 * 4 - 4]
+    shr rax, 1
+    ret
+GetMedian ENDP
+end
